5c74b82804cd6e7d6ad8c59db638c22455439e14
niraj-pawar-webmoob <niraj.pawar@webmobinfo.ch>
2025-10-14 11:51:49 +0530

    exists:bool

--- app/schemas.py
+++ app/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 class EnrollResponse(BaseModel):
     ok: bool
     person_id: str
-    exists:                             # The key denotes if the face is present in the image or not.
+    exists: bool                            # The key denotes if the face is present in the image or not.
     bbox: Optional[List[int]] = None
     debug_image_base64: Optional[str] = None
     error: Optional[str] = None          # <-- NEW: carry error text with 200 status
